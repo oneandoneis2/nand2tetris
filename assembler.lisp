2184b02e904462878a191bdc68c05249d7b6b18a
Dominic Humphries <dominich@broadbean.com>
2017-07-25 15:23:52 +0100

    Simplify loop usage

--- assembler.lisp
+++ assembler.lisp
@@ -3,9 +3,9 @@
 (defvar *source* ())
 
 (with-open-file (src (first *args*))
-  (loop for line = (read-line src nil)
-        while line
-        do (setf *source* (cons line *source*))))
+  (setf *source*
+        (loop for line = (read-line src nil)
+              while line
+              collect line)))
 
-(nreverse *source*)
 (princ *source*)
