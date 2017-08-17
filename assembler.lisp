e8e6488270a02d16a7b8fba16292f3665716af6e
Dominic Humphries <dominich@broadbean.com>
2017-08-17 16:51:56 +0100

    Make file iterable

--- assembler.lisp
+++ assembler.lisp
@@ -4,6 +4,7 @@
 (defclass parse ()
   ((filename :initarg :file
              :reader filename)
+   (line :initform 0 :accessor line)
    (data :initform nil :accessor data)))
 
 ; Define the Parse methods
@@ -16,11 +17,21 @@
                 collect line))))
 
 (defmethod hasMoreCommands ((p parse))
-  (> (length (data p)) 0))
+  (> (- (size p) (line p)) 0))
 
 (defmethod output ((p parse))
   (format t "~{~a~%~}" (data p)))
 
+(defmethod size ((p parse))
+  (length (data p)))
+
+(defmethod advance ((p parse))
+  (if (hasMoreCommands p)
+    (incf (line p)))
+  (nth (line p) (data p)))
+
 ; Objects defined, do the thing
 (defvar *parse* (make-instance 'parse :file (first *args*)))
-(output *parse*)
+(defvar *command* nil)
+(loop while (hasMoreCommands *parse*)
+      do (format t "~a~%" (advance *parse*)))
