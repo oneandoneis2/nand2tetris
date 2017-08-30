f4bac4f60ce815f164adf95e23dc8945e85e0ea0
Dominic Humphries <dominich@broadbean.com>
2017-08-30 13:40:28 +0100

    Use function - doesn't need to be a macro

--- assembler.lisp
+++ assembler.lisp
@@ -132,17 +132,17 @@
 ; We need a mnemonic conversion table. Stick it in a hash
 (defvar *mnemonics* (make-hash-table :test 'equal))
 
-; To drastically reduce repetition, use a macro to popualte the hash
-(defmacro putmnem (pairs)
-  (cons 'progn (loop for (key value) in pairs
-                     collect `(setf (gethash ,key *mnemonics*) ,value))))
-(putmnem (("0"   "0101010") ("1"   "0111111") ("-1"  "0111010") ("D"   "0001100")
-          ("A"   "0110000") ("!D"  "0001101") ("!A"  "0110001") ("-D"  "0001111")
-          ("-A"  "0110011") ("D+1" "0011111") ("A+1" "0110111") ("D-1" "0001110")
-          ("A-1" "0110010") ("D+A" "0000010") ("D-A" "0010011") ("A-D" "0000111")
-          ("D&A" "0000000") ("D|A" "0010101") ("M"   "1110000") ("!M"  "1110001")
-          ("-M"  "1110011") ("M+1" "1110111") ("M-1" "1110010") ("D+M" "1000010")
-          ("D-M" "1010011") ("M-D" "1000111") ("D&M" "1000000") ("D|M" "1010101")))
+; To drastically reduce repetition, use a function to populate the hash
+(defun putmnem (pairs)
+  (loop for (key value) in pairs
+        do (setf (gethash key *mnemonics*) value)))
+(putmnem '(("0"   "0101010") ("1"   "0111111") ("-1"  "0111010") ("D"   "0001100")
+           ("A"   "0110000") ("!D"  "0001101") ("!A"  "0110001") ("-D"  "0001111")
+           ("-A"  "0110011") ("D+1" "0011111") ("A+1" "0110111") ("D-1" "0001110")
+           ("A-1" "0110010") ("D+A" "0000010") ("D-A" "0010011") ("A-D" "0000111")
+           ("D&A" "0000000") ("D|A" "0010101") ("M"   "1110000") ("!M"  "1110001")
+           ("-M"  "1110011") ("M+1" "1110111") ("M-1" "1110010") ("D+M" "1000010")
+           ("D-M" "1010011") ("M-D" "1000111") ("D&M" "1000000") ("D|M" "1010101")))
 
 (defvar *command* nil)
 (loop for line = (current *parse*)
